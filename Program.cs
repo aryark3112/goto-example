84413ef4977bd28949e6e5628938a295e8917d8c
aryark <aryark3112@gmail.com>
2023-07-11 12:11:51 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -10,19 +10,23 @@ namespace goto_example
     {
         static void Main(string[] args)
         {
-            string userName;
-            Again:
-            Console.WriteLine("Enter User Name");
-            userName = Console.ReadLine();
-            if(userName.Length>=6)
-            {
-                Console.WriteLine("Welcome:\t" + userName);
-            }
-            else
-            {
-                Console.WriteLine("Invalid User Name");
-                goto Again;
-            }
+            //string userName;
+            //Again:
+            //Console.WriteLine("Enter User Name");
+            //userName = Console.ReadLine();
+            //if(userName.Length>=6)
+            //{
+            //   Console.WriteLine("Welcome:\t" + userName);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid User Name");
+            //   goto Again;
+            // }
+            StringBuilder stringBuilder = new StringBuilder("Welcome");
+            stringBuilder.Append("To C#");
+            stringBuilder.Append("I am String Builder");
+            Console.WriteLine(stringBuilder);
             Console.ReadKey();
         }
     }
